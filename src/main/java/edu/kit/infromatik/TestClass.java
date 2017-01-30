12f36bf229d3d67efd2fa9d7c2d06cb32f0a10b9
oberacda <david.oberacker@gmail.com>
2017-01-30 18:17:04 +0100

    Initial commit

---
+++ src/main/java/edu/kit/infromatik/TestClass.java
@@ -0,0 +1,20 @@
+package edu.kit.infromatik;
+
+/**
+ * Created by oberacda on 30.01.2017.
+ */
+public class TestClass {
+    private String testMessage;
+
+    public TestClass(final String message) {
+        this.testMessage = message;
+    }
+
+    public String getTestMessage() {
+        return testMessage;
+    }
+
+    public void setTestMessage(String testMessage) {
+        this.testMessage = testMessage;
+    }
+}
---
+++ src/test/java/edu/kit/informatik/test/ClassTest.java
@@ -0,0 +1,26 @@
+package edu.kit.informatik.test;
+
+import edu.kit.infromatik.TestClass;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by oberacda on 30.01.2017.
+ */
+public class ClassTest {
+    @Test
+    public void testGetName() throws Exception {
+        TestClass test = new TestClass("Test");
+        assertThat("Test1", test.getTestMessage(), is("Test"));
+    }
+
+    @Test
+    public void testSetName() throws Exception {
+        TestClass test = new TestClass("Test");
+        assertThat("Test1", test.getTestMessage(), is("Test"));
+        test.setTestMessage("Test2");
+        assertThat("Test2", test.getTestMessage(), is("Test2"));
+    }
+}
