8ce54ebd220a1b829148e6be3a1c5f576819f947
David Oberacker <david.oberacker@student.kit.edu>
2017-02-13 12:33:50 +0100

    PRGExamTask1-24 Modellation Implemented basic Litreture index

--- src/main/java/edu/kit/informatik/management/literature/LiteratureIndex.java
+++ src/main/java/edu/kit/informatik/management/literature/LiteratureIndex.java
@@ -1,11 +1,10 @@
 package edu.kit.informatik.management.literature;
 
-import java.util.Comparator;
 import java.util.Iterator;
 import java.util.TreeSet;
 
 /**
- * Manages the litrature index of a publication.
+ * Manages the literature index of a publication.
  * <p>
  * This class manages a collection of articles cited in the
  * article its a filed of.
@@ -16,34 +15,74 @@ import java.util.TreeSet;
  */
 public class LiteratureIndex {
 
+    //=================Fields==========================
+
     private TreeSet<Article> litratureIndex;
 
-    public LiteratureIndex() {
-        this.litratureIndex = new TreeSet<>(new Comparator<Article>() {
-            @Override
-            public int compare(final Article o1, final Article o2) {
-                Iterator<Author> iterO1 = o1.getAuthors().iterator(),
-                        iterO2 = o2.getAuthors().iterator();
-                while (iterO1.hasNext() && iterO2.hasNext()) {
-                    Author a = iterO1.next();
-                    Author b = iterO2.next();
+    //=================Constructor======================
 
-                    if (a.compareTo(b) == 0) {
-                        continue;
-                    } else {
-                        return a.compareTo(b);
-                    }
+    /**
+     * Creates a new empty literature index.
+     * <p>
+     * A empty literature index has no articles in it.
+     * </p>
+     * <p>
+     * The literature index is sorted with the schema:
+     * <table>
+     * <thead>
+     * <tr>
+     * <td>| ID</td> <td>| 1st Author</td> <td>| 2nd Author</td> <td>| 3rd Author</td>
+     * </tr>
+     * </thead>
+     * <tbody>
+     * <tr>
+     * <td>| 1</td> <td>| A</td> <td>| -</td> <td>| -</td>
+     * </tr>
+     * <tr>
+     * <td>| 2</td> <td>| A</td> <td>| B</td> <td>| -</td>
+     * </tr>
+     * <tr>
+     * <td>| 3</td> <td>| B</td> <td>| -</td> <td>| -</td>
+     * </tr>
+     * <tr>
+     * <td>| 4</td> <td>| B</td> <td>| C</td> <td>| D</td>
+     * </tr>
+     * <tr>
+     * <td>| 5</td> <td>| B</td> <td>| D</td> <td>| -</td>
+     * </tr>
+     * </tbody>
+     * <p>
+     * </table>
+     * </p>
+     */
+    public LiteratureIndex() {
+        this.litratureIndex = new TreeSet<>((o1, o2) -> {
+
+            Iterator<Author> iterO1 = o1.getAuthors().iterator();
+            Iterator<Author> iterO2 = o2.getAuthors().iterator();
+
+            while (iterO1.hasNext() && iterO2.hasNext()) {
+                Author a = iterO1.next();
+                Author b = iterO2.next();
+
+                if (a.compareTo(b) != 0) {
+                    return a.compareTo(b);
                 }
-                if (iterO1.hasNext()) {
-                    return -1;
-                }
-                if (iterO2.hasNext()) {
-                    return 1;
-                }
-                return 0;
             }
+
+            if (iterO1.hasNext()) {
+                return 1;
+            }
+
+            if (iterO2.hasNext()) {
+                return - 1;
+            }
+
+            return - 1;
         });
     }
+
+    //=================Methods==========================
 
     /**
      * Adds a citation to the list.
@@ -52,6 +91,9 @@ public class LiteratureIndex {
      *         the article that was cited.
      */
     public void addCitation(final Article citedArticle) {
-        if (citedArticle.isComplete())
+        if (citedArticle.isComplete()
+                && ! (this.litratureIndex.contains(citedArticle))) {
+            this.litratureIndex.add(citedArticle);
+        }
     }
 }
